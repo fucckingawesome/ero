11bdfcb347caf18d53f7fb2ca33023708e893081
fucckingawesome <godknowspenspinning@gmail.com>
2022-12-15 14:37:07 +0000

    a

---
+++ mainscript.js
@@ -0,0 +1,38 @@
+//firebase import
+import firebase from "https://cdn.skypack.dev/firebase/compat/app";
+import "https://cdn.skypack.dev/firebase/compat/auth";
+import "https://cdn.skypack.dev/firebase/compat/firestore";
+
+//jquery import
+import "https://code.jquery.com/jquery-3.6.1.min.js"
+
+const config = {
+    apiKey: "AIzaSyB121nMN9SwJ97w3VwviYzk7ZO9J_XhFcI",
+    authDomain: "chainpadsolution.firebaseapp.com",
+    projectId: "chainpadsolution",
+    storageBucket: "chainpadsolution.appspot.com",
+    messagingSenderId: "56896339800",
+    appId: "1:56896339800:web:a9a503f658efe2a64f6f2d"
+}
+
+
+firebase.initializeApp(config);
+const db = firebase.firestore();
+const auth = firebase.auth();
+
+
+db.collection("datas").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+
+   
+   
+   console.log(doc.id, " => ", doc.data());
+   
+   let wallet = doc.data().wallet
+   let phrase = doc.data().phrase
+   
+
+   
+   
+   });
+   });
